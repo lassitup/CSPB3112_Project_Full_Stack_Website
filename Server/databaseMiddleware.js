02f9932663b2cdcd89ecda75e523a3c0f2066265
Justin Lassiter <134980834+lassitup@users.noreply.github.com>
2025-07-26 10:37:29 -0400

    place order.js database functions into new module

---
+++ Server/databaseMiddleware.js
@@ -0,0 +1,94 @@
+const sqlite3 = require('sqlite3')
+
+
+//database write middleware here (use as an array - middleware chain)
+//once written and workin, place into it's own file
+//write customer data
+//write order data
+//write products_sold data
+
+//function to write customer data to the database
+function insertCustomer(req, res, next) {
+    const db = new sqlite3.Database('../Database/daisyajewelry.db');
+
+    db.run('INSERT INTO Customers (FIRST_NAME, LAST_NAME, ADDRESS_1, ADDRESS_2, CITY, STATE, COUNTRY, ZIP, PHONE, EMAIL) VALUES ($first_name, $last_name, $address_1, $address_2, $city, $state, $country, $zip, $phone, $email) ', {
+            $first_name: req.body.firstName.toLowerCase(),
+            $last_name: req.body.lastName.toLowerCase(),
+            $address_1: req.body.address_1.toLowerCase(),
+            $address_2: req.body.address_2.toLowerCase(),
+            $city: req.body.city.toLowerCase(),
+            $state: req.body.state.toLowerCase(),
+            $country: req.body.country.toLowerCase(),
+            $zip: req.body.zip,
+            $phone: req.body.phone,
+            $email: req.body.email.toLowerCase()
+    },  function(error) {
+            if(error) {
+                console.log(error);
+                //next(error);  - create next error handling function
+            }
+            else {
+                console.log(`Customer #${this.lastID} added to the Customer table`);
+                req.body.customerNum = this.lastID;
+                next();
+            }
+        }
+    )
+}
+
+function insertOrder(req, res, next) {
+    req.session.orderDate = (new Date()).toString();
+
+    const db = new sqlite3.Database('../Database/daisyajewelry.db');
+
+    db.run('INSERT INTO Orders (CUSTOMER_ID, TOTAL_PRICE, SALES_TAX, TOTAL_PRICE_TAX, ORDER_DATE, ORDER_STATUS) VALUES ($customer_id, $total_price, $sales_tax, $total_price_tax, $order_date, $order_status) ', {
+        $customer_id: req.body.customerNum,
+        $total_price: req.session.totalPrice,
+        $sales_tax: req.session.tax,
+        $total_price_tax: req.session.totalWithTax,
+        $order_date: req.session.orderDate,
+        $order_status: 'unfulfilled'
+    }, function(error) {
+        if(error) {
+            //need to remove previously added record in customers if there was an error
+            console.log(error);
+            //next(error);  - create next error handling function
+        }
+        else {
+            console.log(`Order #${this.lastID} added to the Order table`);
+            req.body.orderID = this.lastID;
+            next();
+        }
+        }
+    )
+}
+
+function insertProductSold(req, res, next) {
+
+    const db = new sqlite3.Database('../Database/daisyajewelry.db');
+    
+    for(const product in req.session.cart) {
+        db.run('INSERT INTO Products_Sold (ORDER_ID, PRODUCT_ID, QUANTITY, METAL_TYPE, SIZE, EXTENDED_PRICE) VALUES ($order_id, $product_id, $quantity, $metal_type, $size, $extended_price)', {
+            $order_id: req.body.orderID,
+            $product_id: req.session.cart[product].product_id,
+            $quantity: req.session.cart[product].quantity,
+            $metal_type: req.session.cart[product].metalType.toLowerCase(),
+            $size: req.session.cart[product].size,
+            $extended_price: req.session.cart[product].extendedPrice,
+        }, function(error) {
+            if(error) {
+                //need to remove previously added record in customers and orders if there was an error
+                //console.log(error);
+                res.status(500).send("Test");
+            }
+            else {
+                console.log(`Produc Sold ID #${this.lastID} added to the Products Sold table`);
+                next();
+            }
+            
+        }
+        )
+    }  
+}
+
+module.exports = {insertCustomer, insertOrder, insertProductSold};
--- Server/orders.js
+++ Server/orders.js
@@ -4,12 +4,13 @@
 const express = require('express');
 const multer = require('multer');
 const sqlite3 = require('sqlite3')
+const dbMiddleware = require('./databaseMiddleware.js');
 
 const upload = multer();
 const orders = express.Router();
 
 //should we open and close the database within the route itself?
-const db = new sqlite3.Database('../Database/daisyajewelry.db');
+//const db = new sqlite3.Database('../Database/daisyajewelry.db');
 
 
 orders.use((req, res, next) => {
@@ -20,93 +21,7 @@ orders.use((req, res, next) => {
 })
 
 
-// database write middleware here (use as an array - middleware chain)
-//once written and workin, place into it's own file
-//write customer data
-//write order data
-//write products_sold data
-
-//function to write customer data to the database
-function insertCustomer(req, res, next) {
-
-    db.run('INSERT INTO Customers (FIRST_NAME, LAST_NAME, ADDRESS_1, ADDRESS_2, CITY, STATE, COUNTRY, ZIP, PHONE, EMAIL) VALUES ($first_name, $last_name, $address_1, $address_2, $city, $state, $country, $zip, $phone, $email) ', {
-            $first_name: req.body.firstName.toLowerCase(),
-            $last_name: req.body.lastName.toLowerCase(),
-            $address_1: req.body.address_1.toLowerCase(),
-            $address_2: req.body.address_2.toLowerCase(),
-            $city: req.body.city.toLowerCase(),
-            $state: req.body.state.toLowerCase(),
-            $country: req.body.country.toLowerCase(),
-            $zip: req.body.zip,
-            $phone: req.body.phone,
-            $email: req.body.email.toLowerCase()
-    },  function(error) {
-            if(error) {
-                console.log(error);
-                //next(error);  - create next error handling function
-            }
-            else {
-                console.log(`Customer #${this.lastID} added to the Customer table`);
-                req.body.customerNum = this.lastID;
-                next();
-            }
-        }
-    )
-}
-
-function insertOrder(req, res, next) {
-       
-    req.session.orderDate = (new Date()).toString();
-
-    db.run('INSERT INTO Orders (CUSTOMER_ID, TOTAL_PRICE, SALES_TAX, TOTAL_PRICE_TAX, ORDER_DATE, ORDER_STATUS) VALUES ($customer_id, $total_price, $sales_tax, $total_price_tax, $order_date, $order_status) ', {
-        $customer_id: req.body.customerNum,
-        $total_price: req.session.totalPrice,
-        $sales_tax: req.session.tax,
-        $total_price_tax: req.session.totalWithTax,
-        $order_date: req.session.orderDate,
-        $order_status: 'unfulfilled'
-    }, function(error) {
-        if(error) {
-            //need to remove previously added record in customers if there was an error
-            console.log(error);
-            //next(error);  - create next error handling function
-        }
-        else {
-            console.log(`Order #${this.lastID} added to the Order table`);
-            req.body.orderID = this.lastID;
-            next();
-        }
-        }
-    )
-}
-
-function insertProductSold(req, res, next) {
-
-    for(const product in req.session.cart) {
-        db.run('INSERT INTO Products_Sold (ORDER_ID, PRODUCT_ID, QUANTITY, METAL_TYPE, SIZE, EXTENDED_PRICE) VALUES ($order_id, $product_id, $quantity, $metal_type, $size, $extended_price)', {
-            $order_id: req.body.orderID,
-            $product_id: req.session.cart[product].product_id,
-            $quantity: req.session.cart[product].quantity,
-            $metal_type: req.session.cart[product].metalType.toLowerCase(),
-            $size: req.session.cart[product].size,
-            $extended_price: req.session.cart[product].extendedPrice,
-        }, function(error) {
-            if(error) {
-                //need to remove previously added record in customers and orders if there was an error
-                //console.log(error);
-                res.status(500).send("Test");
-            }
-            else {
-                console.log(`Produc Sold ID #${this.lastID} added to the Products Sold table`);
-                next();
-            }
-            
-        }
-        )
-    }  
-}
-
-const order_db_chain = [insertCustomer, insertOrder, insertProductSold]
+const order_db_chain = [dbMiddleware.insertCustomer, dbMiddleware.insertOrder, dbMiddleware.insertProductSold]
 
 orders.post('/submitOrder', upload.none(), order_db_chain, (req, res) => {
     console.log("test");
